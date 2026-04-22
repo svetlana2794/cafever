2a547799be3bf233f6ea377db114c4f9e13ba889
svetlana2794 <110475110+svetlana2794@users.noreply.github.com>
2026-04-23 00:06:08 +0300

    Add files via upload

--- src/components/Acknow.vue
+++ src/components/Acknow.vue
@@ -1,57 +1,58 @@
 <script setup>
-import Links from "./Links.vue"
+import Links from './Links.vue'
 
-import { useRouter } from "vue-router"
+import { useRouter } from 'vue0router'
 
-import { useCartStore } from "../stores/storeCart.js"
-import { useOrderStore } from "../stores/storeOrder.js"
+import { useCartStore } from '../stores/storeCart.js'   
+import { useOrderStore } from '../stores/storeOrder.js'
 
 const storeCart=useCartStore()
 const storeOrder=useOrderStore()
 const router=useRouter()
 
-function goToMain() {
-storeCart.cart.clear()
-storeOrder.step=0
-router.push("/")
+functiongoToMain() {
+    storeCart.cart.clear()
+    storeOrder.step=0
+    router.push('/')
 }
-
 </script>
 
 <template>
-<div class="p-6 bg-[#ede0d4] rounded-xl m-2 h-full">
-<h2 class="text-[#7f5539] text-2xl md:text-4xl xl:text-6xl text-center font-[Futura]">
-<span class="float-left">Шаг 3</span>
-Подтверждение заказа
-</h2>
 
-<Transition name="fade" mode="out-in">
+<div class='p-6 bg-[#ede0d4] rounded-xl m-2 h-full'>
 
-<div v-if="storeOrder.status=='loading'" class="text-center text-2xl my-4">
+<h2 class='text-[#7f5539] text-3xl text-center font-[Futura]'>Шаг 3. Подтверждение заказа</h2>
+
+<Transition name='fade'mode='out-in'>
+
+<div v-if="storeOrder.status=='loading'" class='text-center text-2xl my-4'>
 Заказ обрабатывается...<br>
-<span class="block my-10">
-<font-awesome-icon icon="fa-solid fa-spinner" size="2xl" style="color: #7f5539" class="spin"/>
+
+<span class='block my-10'>
+<font-awesome-icon icon='fa-solid fa-spinner' size='2xl' style='color: #7f5539' class='spin'/>
 </span>
 </div>
-<div v-else-if="storeOrder.status=='ok'" class="text-2xl text-center my-4">Ваш заказ успешно передан.
-<br>
-<span class="my-6 block">
-Номер заказа: {{storeOrder.numOrder}}
-</span>
-<button @click="goToMain" class="rounded-full py-2 px-10 text-2xl block mx-auto my-8 bg-[#b23a46] hover:bg-[#da1e37] text-white">ОК</button>
+
+<div v-else-if="storeOrder.status='ok'" class='text-2xl text-center my-4'>
+Ваш заказ успешно передан.<br>
+
+<span class='my-6 block'>Номер заказа: {{storeOrder.numOrder}}</span>
+
+<button @click='goToMain' class='rounded-full py-2 px-10 text-2xl block mx-auto my-8 bg-[#b23a46] hover:bg-[#da1e37] text-white'>OK</button>
 </div>
-<div v-else-if="storeOrder.status=='error'" class="text-2xl text-center my-10">
-<p>К сожалению оформление заказа на сайте временно не доступно. Попробуйте позже или другим способом:</p>
+
+<div v-else-if="storeOrder.status=='error'" class='text-2xl text-center my-10'>
+<p>К сожалению оформоение заказа на сайте временно не доступно. Попробуйте позже или другим способом.</p>
 <Links/>
 </div>
 
 </Transition>
 
 </div>
+
 </template>
 
 <style scoped>
-
 .spin {
 animation: spin 1s linear infinite;
 }
@@ -60,9 +61,9 @@ animation: spin 1s linear infinite;
 from {
 transform: rotate(0deg)
 }
+
 to {
 transform: rotate(360deg)
 }
 }
-
 </style>
