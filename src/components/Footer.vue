e1c29d223431ed030b2a58980599848f6fbb5f1a
svetlana2794 <110475110+svetlana2794@users.noreply.github.com>
2026-05-23 17:44:10 +0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-<footer class="text-center p-2 mt-2">
+<footer class="text-center text-white p-2 mt-2">
 <address>
 2025 &copy; Svetlana Bolo, email: <a href="mailto:sapsan-94@mail.ru">sapsan-94@mail.ru</a>
 </address>
