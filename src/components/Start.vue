2c419d05e86e2e7c8037ae650395888c4bea592e
svetlana2794 <110475110+svetlana2794@users.noreply.github.com>
2026-05-23 18:25:28 +0300

    Update Start.vue

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -12,7 +12,7 @@ const router=useRouter()
     </p>
 <p class="pt-8 pb-4 font-[Futura]">Свет, простор и теплая атмосфера.</p>
 <p class="py-4 font-[Futura]">Все, за что мы любим Италию - в одном месте.</p>
-<RouterLink class="bg-[#b23a46] text-white hover:bg-[#da1e37] px-4 py-2 block mx-auto mb-6 mt-20 w-1/2 text-center rounded-full" to="/menu">Меню</RouterLink>
+<RouterLink class="bg-[#b23a46] text-white hover:bg-[#da1e37] px-4 py-2 block mx-auto mb-6 mt-10 w-1/2 text-center rounded-full" to="/menu">Меню</RouterLink>
 </div>
 </template>
 
